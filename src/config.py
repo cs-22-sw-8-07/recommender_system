b671f7bd2101f306ac41795ee89adc4398ab53c8
Jeppe Lindberg <jepp3441@gmail.com>
2022-05-02 13:08:18 +0200

    New recommender unit tests

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 
 
 def load_config(config_file_name: str) -> ConfigParser:
-    base_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+    base_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir)
     config_file = os.path.join(base_folder, config_file_name)
 
     if not os.path.exists(config_file) or not os.path.isfile(config_file):
